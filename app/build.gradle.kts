1a408b5841cd3eff6dd5f96a60d10d3041b874ee
GlucoPlan <stariy2003@gmail.com>
2026-04-17 13:59:34 +0300

    Configure signing for release build
    Add signing configuration for release build type

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,21 @@ android {
     }
 
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("keystore.jks")
+            storePassword = System.getenv("KEYSTORE_PASSWORD")
+            keyAlias = System.getenv("KEY_ALIAS")
+            keyPassword = System.getenv("KEY_PASSWORD")
+        }
+    }
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }
+    
+
 
     buildFeatures {
         compose = true
